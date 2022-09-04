a9e03e5f93a407901c7a4ce884e00b9e69e51786
Lucas <lucas_crippa@hotmail.com>
2022-09-04 13:58:50 -0300

    Insert implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -8,7 +8,7 @@ public class Seller implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 
-	private int id;
+	private Integer id;
 	private String name;
 	private String email;
 	private Date birthdate;
@@ -20,7 +20,7 @@ public class Seller implements Serializable {
 
 	}
 
-	public Seller(int id, String name, String email, Date birthdate, double baseSalary, Department department) {
+	public Seller(Integer id, String name, String email, Date birthdate, double baseSalary, Department department) {
 
 		this.id = id;
 		this.name = name;
@@ -30,11 +30,11 @@ public class Seller implements Serializable {
 		this.department = department;
 	}
 
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
